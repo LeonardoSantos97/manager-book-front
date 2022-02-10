83eb39cbaddeade09582d23f872a87ed4c5cfe85
Leonardo Santos <leonardopsantos97@gmail.com>
2022-02-10 13:22:29 -0300

    Delete implement

--- src/app/components/admin/admin-delete/admin-delete.component.ts
+++ src/app/components/admin/admin-delete/admin-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Admin } from 'src/app/models/admin';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin-delete',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDeleteComponent implements OnInit {
 
-  constructor() { }
+  admin: Admin = {
+    id:         '',
+    nome:       '',
+    usuario:      '',
+    senha:      '',
+    perfis:     [],
+    dataCriacao: ''
+  }
+
+  constructor(
+    private service: AdminService,
+    private toast:    ToastrService,
+    private router:          Router,
+    private route:   ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    this.admin.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+  findById(): void {
+    this.service.findById(this.admin.id).subscribe(resposta => {
+      resposta.perfis = []
+      this.admin = resposta;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.admin.id).subscribe(() => {
+      this.toast.success('Técnico deletado com sucesso', 'Delete');
+      this.router.navigate(['admins'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
   }
 
 }
