9ca92c2ed3dc4aea5ee2973765c0656ae31bdbcb
Leonardo Santos <leonardopsantos97@gmail.com>
2022-02-09 23:38:34 -0300

    Created models

--- src/app/components/admin/admin-create/admin-create.component.ts
+++ src/app/components/admin/admin-create/admin-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/admin';
+import { TecnicoService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin-create',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCreateComponent implements OnInit {
 
-  constructor() { }
+  admin: Admin = {
+    id:         '',
+    nome:       '',
+    usuario:      '',
+    senha:      '',
+    perfis:     [],
+    dataCriacao: ''
+  }
+
+  nome: FormControl =  new FormControl(null, Validators.minLength(3));
+  usuario: FormControl =        new FormControl(null, Validators.usuario);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
+
+
+  constructor(
+    private service: AdminService,
+    private toast:    ToastrService,
+    private router:          Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  create(): void {
+    this.service.create(this.admin).subscribe(() => {
+      this.toast.success('Técnico cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(['admins'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  addPerfil(perfil: any): void {
+    if(this.admin.perfis.includes(perfil)) {
+      this.admin.perfis.splice(this.admin.perfis.indexOf(perfil), 1);
+    } else {
+      this.admin.perfis.push(perfil);
+    }
+    
+  }
+  
+  validaCampos(): boolean {
+    return this.nome.valid 
+     && this.usuario.valid && this.senha.valid
+  }
 }
---
+++ src/app/models/livro.ts
@@ -0,0 +1,10 @@
+export interface Livro {
+    id?:                any;
+    dataAbertura?:   string;
+    dataFechamento?: string;
+    titulo:          string;
+    admin:            any;
+    cliente:            any;
+    nomeCliente:     string;
+    nomeAdmin:     string;
+ }
