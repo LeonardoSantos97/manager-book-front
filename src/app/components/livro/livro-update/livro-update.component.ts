5f8e2631732ce9407916dd87235a4d0324f7b402
Leonardo Santos <leonardopsantos97@gmail.com>
2022-02-10 15:29:22 -0300

    HTML and css livro-update created

--- src/app/components/livro/livro-update/livro-update.component.ts
+++ src/app/components/livro/livro-update/livro-update.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Livro } from 'src/app/models/livro';
+import { Cliente } from 'src/app/models/cliente';
+import { Admin } from 'src/app/models/admin';
+import { LivroService } from 'src/app/services/livro.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-livro-update',
@@ -7,9 +16,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivroUpdateComponent implements OnInit {
 
-  constructor() { }
+  livro: Livro = {
+    titulo:      '',
+    admin:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeAdmin: '',
+    //author
+  }
+
+  clientes: Cliente[] = []
+  admins: Admin[] = []
+
+  titulo:     FormControl = new FormControl(null, [Validators.required]);
+  admin:    FormControl = new FormControl(null, [Validators.required]);
+  cliente:    FormControl = new FormControl(null, [Validators.required]);
+
+  constructor(
+    private livroService: LivroService,
+    private clienteService: ClienteService,
+    private adminService: AdminService,
+    private toastService:    ToastrService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.findAllClientes();
+    this.findAllAdmins();
   }
+
+  update(): void {
+    this.livroService.update(this.livro).subscribe(resposta => {
+      this.toastService.success('Livro atualizado com sucesso', 'Livro Atualizado');
+      this.router.navigate(['livros']);
+    }, ex => {
+      console.log(ex);
+      
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
+
+  findAllAdmins(): void {
+    this.adminService.findAll().subscribe(resposta => {
+      this.admins = resposta;
+    })
+  }
+  
 
 }
