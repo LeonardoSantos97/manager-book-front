9b8761696c2ce6dda599c9f7a948994b45e591b3
LeonardoSantos97 <leonardopsantos97@gmail.com>
2022-02-23 17:22:03 -0300

    replacement back

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,7 @@
 export const API_CONFIG = {
     baseUrl : 'https://managerbook-prod.app.com'
+    // baseUrl : 'https://managerbook-prod.herokuapp.com/'
+    // baseUrl : 'https://books-prod.herokuapp.com/'
+    // baseUrl : 'https://leonardosantos97.github.io/book-back/'
+    // baseUrl :'https://booksmanager-prod'
  } 
