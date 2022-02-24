3372e53012065ad9df6561e90ef6ca1eb6a7d2a5
LeonardoSantos97 <leonardopsantos97@gmail.com>
2022-02-24 11:31:54 -0300

    change baseurl

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,7 +1,8 @@
 export const API_CONFIG = {
-    baseUrl : 'https://managerbook-prod.app.com'
-    // baseUrl : 'https://managerbook-prod.herokuapp.com/'
+    // baseUrl : 'https://managerbook-prod.app.com'
+    baseUrl : 'https://managerbook-prod.herokuapp.com/'
     // baseUrl : 'https://books-prod.herokuapp.com/'
     // baseUrl : 'https://leonardosantos97.github.io/book-back/'
     // baseUrl :'https://booksmanager-prod'
  } 
+ 
